d2f1c60d471b03a61be2218905d9aa4d2f560933
Rishi Bhargava <rishi.bhargava@gmail.com>
2015-08-12 12:54:29 -0700

    Changes to details js for calling backend.

--- static/master/js/modules/details.js
+++ static/master/js/modules/details.js
@@ -13,6 +13,35 @@
         $('#for').text('file ' + qParts['f']);
       } else if (qParts['m']) {
 
+        $.ajax({
+          type: 'GET',
+          url: '/work',
+          data: {u: qParts['u'], f: qParts['f'], m: qParts['m'], c: qParts['c']},
+          headers: {'X-XSRF-TOKEN': Cookies.get('XSRF')},
+          dataType: 'json',
+          contentType: 'application/json; charset=utf-8',
+          success: function(){
+            toastr.options = {
+              "closeButton": false,
+              "debug": false,
+              "newestOnTop": false,
+              "progressBar": false,
+              "positionClass": "toast-top-center",
+              "preventDuplicates": true,
+              "onclick": null,
+              "showDuration": "300",
+              "hideDuration": "1000",
+              "timeOut": "5000",
+              "extendedTimeOut": "1000",
+              "showEasing": "swing",
+              "hideEasing": "linear",
+              "showMethod": "fadeIn",
+              "hideMethod": "fadeOut"
+            };
+            toastr["success"]("ajax request succeded");
+          }
+        });
+
       }
     }
   }
