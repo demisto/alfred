248f14bfcd58753c3241c69bafa0b521f2661658
slavikm <slavik.markovich@gmail.com>
2015-08-21 00:59:30 -0700

    Minor fix for dedup log

--- bot/dedup.go
+++ bot/dedup.go
@@ -30,7 +30,7 @@ func (d *Dedup) Start() {
 	for {
 		msg, err := d.q.PopMessage(0)
 		if err != nil || msg == nil {
-			logrus.Info("Stoping DEDUP process - %v, %v", err, msg)
+			logrus.Infof("Stoping DEDUP process - %v, %v", err, msg)
 			return
 		}
 		logrus.Debugf("Deduping message %s\n", msg.MessageID)
