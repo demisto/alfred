e1516f04bb180cc8ef33025232d96d1aa647d436
Rishi Bhargava <rishi.bhargava@gmail.com>
2015-09-24 15:53:05 -0700

    Screen shots addition to home page.

--- static/master/js/script.js
+++ static/master/js/script.js
@@ -135,6 +135,32 @@
     }
   });
 
+// appshot owl-carousel
+  $('#appshots').owlCarousel({
+
+      autoPlay: 3000,
+      items: 2,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3],
+      margin:          10,
+      responsiveClass: true,
+      responsive:      {
+          0: {
+              items: 1,
+              nav:   false
+          },
+          500: {
+              items: 2,
+              nav:   false
+          },
+          1000: {
+              items: 4,
+              nav:   false,
+              loop:  false
+          }
+      }
+  });
+
 
   // Self close navbar on mobile click
   // -----------------------------------
